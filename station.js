017f5a8eaf65926b407a79e6b4fe8684775fe67c
j9magayanes <j9magayanes>
2024-12-05 12:45:23 +0100

    fix: latest working version before design change

---
+++ station.js
@@ -0,0 +1,41 @@
+{
+    "stations": [
+      {
+        "station": "Berlin Brandenburg",
+        "id": "427",
+        "distanceInKm": "4.0",
+        "results": "24644"
+      },
+      {
+        "station": "Berlin-Adlershof",
+        "id": "395",
+        "distanceInKm": "4.1",
+        "results": "2010"
+      },
+      {
+        "station": "Berlin-Treptow",
+        "id": "434",
+        "distanceInKm": "6.7",
+        "results": "1643"
+      },
+      {
+        "station": "Berlin-Lichterfelde (Süd)",
+        "id": "417",
+        "distanceInKm": "16.3",
+        "results": "3623"
+      },
+      {
+        "station": "Berlin-Dahlem (LFAG)",
+        "id": "402",
+        "distanceInKm": "17.2",
+        "results": "4217"
+      },
+      {
+        "station": "Potsdam (Säkularstation)",
+        "id": "3988",
+        "distanceInKm": "32.9",
+        "results": "195"
+      }
+    ]
+  }
+  
